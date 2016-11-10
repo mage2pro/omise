29466e2cdc320ee5cdaf3c4babf51a58135c3866
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-10 07:31:46 +0300

    no message

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,11 @@
+var config = {
+	paths: {
+		// 2016-03-02
+		// Хитрый трюк: «?» позволяет избежать автоматического добаления расширения «.js».
+		// https://coderwall.com/p/y4vk_q/requirejs-and-external-scripts
+		// 2016-11-10
+		// https://www.omise.co/collecting-card-information#a-full-fledged-example
+		// https://github.com/omise/omise.js
+		'Dfe_Omise/API': 'https://cdn.omise.co/omise'
+	}
+};
---
+++ view/frontend/web/main.js
@@ -0,0 +1,37 @@
+// 2016-11-10
+define ([
+	'df'
+	,'Df_Checkout/js/data'
+	,'Df_Payment/card'
+	,'Dfe_Omise/API'
+	,'jquery'
+	,'Magento_Payment/js/model/credit-card-validation/credit-card-data'
+], function(df, dfc, parent, API, $, creditCardData) {'use strict'; return parent.extend({
+	/**
+	 * 2016-11-10
+	 * @returns {String[]}
+	 */
+	getCardTypes: function() {return ['VI', 'MC', 'AE', 'JCB', 'DI', 'DN', 'CUN'];},
+	/**
+	 * 2016-11-10
+	 * @return {Object}
+	*/
+	initialize: function() {
+		this._super();
+		if (this.isTest()) {
+		}
+		return this;
+	},
+	/**
+	 * 2016-11-10
+	 * @override
+	 * @see https://github.com/magento/magento2/blob/2.1.0/app/code/Magento/Checkout/view/frontend/web/js/view/payment/default.js#L127-L159
+	 * @used-by https://github.com/magento/magento2/blob/2.1.0/lib/web/knockoutjs/knockout.js#L3863
+	 * @param {this} _this
+	*/
+	placeOrder: function(_this) {
+		if (this.validate()) {
+			this.placeOrderInternal();
+		}
+	}
+});});
