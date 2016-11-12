d8c72ab38d8a530407ac7a49040a815217867364
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-12 09:24:25 +0300

    Omise.createToken

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,8 +1,5 @@
 var config = {
 	paths: {
-		// 2016-03-02
-		// Хитрый трюк: «?» позволяет избежать автоматического добаления расширения «.js».
-		// https://coderwall.com/p/y4vk_q/requirejs-and-external-scripts
 		// 2016-11-10
 		// https://www.omise.co/collecting-card-information#a-full-fledged-example
 		// https://github.com/omise/omise.js
--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -12,7 +12,7 @@ define([
 	Omise
 ) {'use strict'; return parent.extend({
 	/**
-	 * 2016-11-10
+	 * 2016-11-12
 	 * https://www.omise.co/which-credit-cards-does-omise-accept
 	 * @returns {String[]}
 	 */
@@ -28,7 +28,7 @@ define([
 		return this;
 	},
 	/**
-	 * 2016-11-10
+	 * 2016-11-12
 	 * @override
 	 * @see https://github.com/magento/magento2/blob/2.1.0/app/code/Magento/Checkout/view/frontend/web/js/view/payment/default.js#L127-L159
 	 * @used-by https://github.com/magento/magento2/blob/2.1.0/lib/web/knockoutjs/knockout.js#L3863
@@ -36,59 +36,39 @@ define([
 	*/
 	placeOrder: function(_this) {
 		if (this.validate()) {
-			if (!this.isNewCardChosen()) {
+			Omise.createToken('card',
+				{
+					expiration_month: this.dfCardExpirationMonth()
+					,expiration_year: this.dfCardExpirationYear()
+					// 2016-11-12
+					// https://www.omise.co/tokens-api#tokens-create
+					// «The cardholder name as printed on the card.»
+					// This parameter is required:
+					// https://www.omise.co/omise-js-api#createtoken(type,-object,-callback)
+					,name: 'TEST TEST'
+					,number: this.dfCardNumber()
+					,security_code: this.dfCardVerification()
+				},
 				/**
-				 * 2016-08-23
-				 * Идентификаторы карт начинаются с приставки «card_»
-				 * (например: «card_18lGFRFzKb8aMux1Bmcjsa5L»),
-				 * а идентификаторы токенов — с приставки «tok_»
-				 * (например: «tok_18lWSWFzKb8aMux1viSqpL5X»),
-				 * тем самым по приставке мы можем отличить карты от токенов,
-				 * и поэтому для карт и токенов мы можем использовать одну и ту же переменную.
+				 * 2016-11-12
+				 * @param {Number} status
+				 * @param {Object} response
 				 */
-				this.token = this.currentCard();
-				this.placeOrderInternal();
-			}
-			else {
-				// 2016-03-02
-				// https://stripe.com/docs/custom-form#step-2-create-a-single-use-token
-				/**
-				 * 2016-03-07
-				 * https://support.stripe.com/questions/which-cards-and-payment-types-can-i-accept-with-stripe
-				 * Which cards and payment types can I accept with Stripe?
-				 * With Stripe, you can charge almost any kind of credit or debit card:
-				 * U.S. businesses can accept:
-				 * 		Visa, MasterCard, American Express, JCB, Discover, and Diners Club.
-				 * Australian, Canadian, European, and Japanese businesses can accept:
-				 * 		Visa, MasterCard, and American Express.
-				 */
-				Stripe.card.createToken(
-					{
-						cvc: this.dfCardVerification()
-						,exp_month: this.dfCardExpirationMonth()
-						,exp_year: this.dfCardExpirationYear()
-						,number: this.dfCardNumber()
-					},
-					/**
-					 * 2016-03-02
-					 * @param {Number} status
-					 * @param {Object} response
-					 */
-					function(status, response) {
-						if (200 === status) {
-							// 2016-03-02
-							// https://stripe.com/docs/custom-form#step-3-sending-the-form-to-your-server
-							_this.token = response.id;
-							_this.placeOrderInternal();
-						}
-						else {
-							// 2016-03-02
-							// https://stripe.com/docs/api#errors
-							_this.showErrorMessage(response.error.message);
-						}
+				function(status, response) {
+					if (200 === status) {
+						// 2016-03-02
+						// https://stripe.com/docs/custom-form#step-3-sending-the-form-to-your-server
+						_this.token = response.id;
+						debugger;
+						//_this.placeOrderInternal();
 					}
-				);
-			}
+					else {
+						// 2016-11-12
+						// https://www.omise.co/omise-js-api#createtoken(type,-object,-callback)
+						_this.showErrorMessage(response.message);
+					}
+				}
+			);
 		}
 	}
 });});
