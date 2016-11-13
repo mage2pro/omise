939a30e8c1f370403fcd3ee33315c82afc2d418a
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-13 03:24:34 +0300

    no message

--- Method.php
+++ Method.php
@@ -1,6 +1,7 @@
 <?php
 // 2016-11-10
 namespace Dfe\Omise;
+use Magento\Sales\Model\Order as O;
 /** @method Settings s() */
 class Method extends \Df\Payment\Method {
 	/**
@@ -22,6 +23,22 @@ class Method extends \Df\Payment\Method {
 	 * @return string[]
 	 */
 	protected function iiaKeys() {return [self::$TOKEN];}
+
+	/**
+	 * 2016-11-13
+	 * @override
+	 * @see \Df\Payment\Method::initialize()
+	 * @param string $paymentAction
+	 * @param object $stateObject
+	 * https://github.com/magento/magento2/blob/2.1.0/app/code/Magento/Sales/Model/Order/Payment.php#L336-L346
+	 * @see \Magento\Sales\Model\Order::isPaymentReview()
+	 * https://github.com/magento/magento2/blob/2.1.0/app/code/Magento/Sales/Model/Order.php#L821-L832
+	 * @return $this
+	 */
+	public function initialize($paymentAction, $stateObject) {
+		$stateObject['state'] = O::STATE_PAYMENT_REVIEW;
+		return $this;
+	}
 
 	/**
 	 * 2016-11-10
