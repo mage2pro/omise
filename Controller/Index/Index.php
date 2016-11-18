06786eddf2f655fcf8a27d50002b370211424ef1
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-18 14:24:33 +0300

    no message

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -2,6 +2,8 @@
 // 2016-11-10
 namespace Dfe\Omise\Controller\Index;
 use Df\Framework\Controller\Result\Json;
+//use Dfe\Omise\Handler;
+use Dfe\Omise\Settings as S;
 class Index extends \Magento\Framework\App\Action\Action {
 	/**
 	 * 2016-11-10
@@ -9,5 +11,19 @@ class Index extends \Magento\Framework\App\Action\Action {
 	 * @see \Magento\Framework\App\Action\Action::execute()
 	 * @return Json
 	 */
-	public function execute() {return Json::i('OK');}
+	public function execute() {return df_leh(function(){
+		S::s()->init();
+		dfp_report($this, df_ruri(), 'request-uri');
+		dfp_report($this, df_request(), 'request-params');
+		return Json::i('OK');
+		//return Json::i(Handler::p(df_json_decode(@file_get_contents($this->file()))));
+	});}
+
+	/**
+	 * 2016-11-17
+	 * @return string
+	 */
+	private function file() {return
+		df_my_local() ? df_test_file($this, 'charge.capture.json') : 'php://input'
+	;}
 }
