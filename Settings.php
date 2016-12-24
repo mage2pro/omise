824c58e6e3c36cf213f3c13e5186ad2ab52f4640
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-12-24 10:41:20 +0300

    1.0.5

--- Settings.php
+++ Settings.php
@@ -4,6 +4,13 @@ namespace Dfe\Omise;
 /** @method static Settings s() */
 final class Settings extends \Df\Payment\Settings\StripeClone {
 	/**
+	 * 2016-12-23
+	 * «Whether the 3D Secure Validation is enabled for your Omise account»
+	 * @return bool
+	 */
+	public function _3DS() {return $this->testableB();}
+
+	/**
 	 * 2016-11-13
 	 * https://github.com/omise/omise-php#the-code
 	 * @return void
