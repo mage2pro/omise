0d11c1306c7f29bb7e625043f867057c69835e92
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-01-12 07:35:19 +0300

    no message

--- Webhook.php
+++ Webhook.php
@@ -1,4 +1,22 @@
 <?php
 // 2017-01-09
 namespace Dfe\Omise;
-abstract class Webhook extends \Df\StripeClone\Webhook {}
+abstract class Webhook extends \Df\StripeClone\Webhook {
+	/**
+	 * 2017-01-12
+	 * @override
+	 * @see \Df\Payment\Webhook::parentIdRawKey()
+	 * @used-by \Df\Payment\Webhook::parentIdRaw()
+	 * @return string
+	 */
+	final protected function parentIdRawKey() {return 'data/id';}
+
+	/**
+	 * 2017-01-12
+	 * @override
+	 * @see \Df\StripeClone\Webhook::roPath()
+	 * @used-by \Df\StripeClone\Webhook::ro()
+	 * @return string
+	 */
+	final protected function roPath() {return 'data';}
+}
