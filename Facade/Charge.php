74552064dc74d4cde6d3792645497562a5a72147
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-10 20:44:28 +0300

    no message

---
+++ Facade/Charge.php
@@ -0,0 +1,16 @@
+<?php
+namespace Dfe\Stripe\Facade;
+use Dfe\Omise\Exception\Charge as E;
+use OmiseCharge as C;
+// 2017-02-10
+final class Charge extends \Df\StripeClone\Facade\Charge {
+	/**
+	 * 2017-02-10
+	 * @override
+	 * @see \Df\StripeClone\Facade\Charge::create()
+	 * @used-by \Df\StripeClone\Method::chargeNew()
+	 * @param array(string => mixed) $p
+	 * @return C
+	 */
+	public function create(array $p) {return E::assert(C::create($p), $p);}
+}
