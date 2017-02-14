a8a2dd2d14c1fa9ba60b07571687e01499e6a645
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-14 05:51:29 +0300

    no message

--- WebhookF.php
+++ WebhookF.php
@@ -7,6 +7,9 @@ final class WebhookF extends \Df\StripeClone\WebhookF {
 	 * Как и у Stripe, это ключ  отсутствует в логе события на странице события в интерфейсе Omise.
 	 * Например: https://dashboard.omise.co/test/events/evnt_test_56mt7g5ehb93e8lzg7g
 	 * Но реально этот ключ присутствует в запросе.
+	 * 2017-02-14
+	 * [Omise] An example of the «charge.capture» event (being sent to a webhook)
+	 * https://mage2.pro/t/2746
 	 * @override
 	 * @see \Df\StripeClone\WebhookF::typeKey()
 	 * @used-by \Df\StripeClone\WebhookF::_class()
