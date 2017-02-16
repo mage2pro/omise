3b10af7c7c8943d738cef565fd7b5d90c79b9d2b
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-16 15:17:50 +0300

    no message

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -9,6 +9,7 @@ define([
 	// Omise requires the cardholder name:
 	// https://www.omise.co/omise-js-api#createtoken(type,-object,-callback)
 	defaults: {df: {card: {requireCardholder: true}}},
+
 	/**
 	 * 2016-11-12
 	 * https://www.omise.co/which-credit-cards-does-omise-accept
@@ -17,6 +18,7 @@ define([
 	 * @returns {String[]}
 	 */
 	getCardTypes: function() {return ['VI', 'MC', 'JCB'];},
+
 	/**
 	 * 2016-11-10
 	 * @override
@@ -27,6 +29,7 @@ define([
 		Omise.setPublicKey(this.publicKey());
 		return this;
 	},
+
 	/**
 	 * 2016-12-24
 	 * 3D Secure redirection.
@@ -40,60 +43,67 @@ define([
 			: this._super()
 		;
 	},
-	/**
-	 * 2016-11-12
+
+    /**
+	 * 2017-02-16
 	 * @override
-	 * @see https://github.com/magento/magento2/blob/2.1.0/app/code/Magento/Checkout/view/frontend/web/js/view/payment/default.js#L127-L159
-	 * @used-by https://github.com/magento/magento2/blob/2.1.0/lib/web/knockoutjs/knockout.js#L3863
-	 * @param {this} _this
-	*/
-	placeOrder: function(_this) {
-		if (this.validate()) {
-			if (!this.isNewCardChosen()) {
-				/**
-				 * 2016-08-23
-				 * Идентификаторы карт начинаются с приставки «card_»
-				 * (например: «card_18lGFRFzKb8aMux1Bmcjsa5L»),
-				 * а идентификаторы токенов — с приставки «tok_»
-				 * (например: «tok_18lWSWFzKb8aMux1viSqpL5X»),
-				 * тем самым по приставке мы можем отличить карты от токенов,
-				 * и поэтому для карт и токенов мы можем использовать одну и ту же переменную.
-				 */
-				this.token = this.currentCard();
-				this.placeOrderInternal();
-			}
-			else {
-				Omise.createToken('card',
-					{
-						expiration_month: this.creditCardExpMonth()
-						,expiration_year: this.creditCardExpYear()
-						// 2016-11-12
-						// https://www.omise.co/tokens-api#tokens-create
-						// «The cardholder name as printed on the card.»
-						// This parameter is required:
-						// https://www.omise.co/omise-js-api#createtoken(type,-object,-callback)
-						,name: this.cardholder()
-						,number: this.creditCardNumber()
-						,security_code: this.creditCardVerificationNumber()
-					},
-					/**
-					 * 2016-11-12
-					 * @param {Number} status
-					 * @param {Object} response
-					 */
-					function(status, response) {
-						if (200 === status) {
-							_this.token = response.id;
-							_this.placeOrderInternal();
-						}
-						else {
-							// 2016-11-12
-							// https://www.omise.co/omise-js-api#createtoken(type,-object,-callback)
-							_this.showErrorMessage(response.message);
-						}
-					}
-				);
-			}
-		}
-	}
+	 * @see ...
+	 * @used-by placeOrder()
+	 * @param {Object|Number} status
+	 * @returns {Boolean}
+	 */
+	tokenCheckStatus: function(status) {return 200 === status;},
+
+    /**
+	 * 2017-02-16
+	 * @override
+	 * @see ...
+	 * @used-by placeOrder()
+	 * @param {Object} params
+	 * @param {Function} callback
+	 * @returns {Function}
+	 */
+	tokenCreate: function(params, callback) {return Omise.createToken('card', params, callback);},
+
+    /**
+	 * 2017-02-16
+	 * https://www.omise.co/omise-js-api#createtoken(type,-object,-callback)
+	 * @override
+	 * @see ...
+	 * @used-by placeOrder()
+	 * @param {Object|Number} status
+	 * @param {Object} resp
+	 * @returns {String}
+	 */
+	tokenErrorMessage: function(status, resp) {return resp.message;},
+
+    /**
+	 * 2017-02-16
+	 * @override
+	 * @see ...
+	 * @used-by placeOrder()
+	 * @param {Object} resp
+	 * @returns {String}
+	 */
+	tokenFromResponse: function(resp) {return resp.id;},
+
+    /**
+	 * 2017-02-16
+	 * @override
+	 * @see ...
+	 * @used-by placeOrder()
+	 * @returns {Object}
+	 */
+	tokenParams: function() {return {
+		expiration_month: this.creditCardExpMonth()
+		,expiration_year: this.creditCardExpYear()
+		// 2016-11-12
+		// https://www.omise.co/tokens-api#tokens-create
+		// «The cardholder name as printed on the card.»
+		// This parameter is required:
+		// https://www.omise.co/omise-js-api#createtoken(type,-object,-callback)
+		,name: this.cardholder()
+		,number: this.creditCardNumber()
+		,security_code: this.creditCardVerificationNumber()
+	};}
 });});
