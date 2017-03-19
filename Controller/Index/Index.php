ecf15f03f465c868e60699c911146e3b06e5c2b8
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-19 18:04:24 +0300

    1.7.1

--- Controller/Index/Index.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace Dfe\Omise\Controller\Index;
-// 2016-11-10
-/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
-class Index extends \Df\Payment\W\Action {}
