9d40af75211d2ca9035d1cef7c7b46190df1b05d
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-22 06:31:24 +0300

    no message

--- Init/Action.php
+++ Init/Action.php
@@ -9,21 +9,12 @@ final class Action extends \Df\Payment\Init\Action {
 	/**
 	 * 2016-12-24
 	 * @override
-	 * @see \Df\Payment\Init\Action::redirectNeeded()
-	 * @used-by \Df\Payment\Init\Action::action()
-	 * @return bool
-	 */
-	protected function redirectNeeded() {return $this->s()->_3DS();}
-	
-	/**
-	 * 2016-12-24
-	 * @override
 	 * @see \Df\Payment\Init\Action::redirectUrl()
 	 * @used-by \Df\Payment\Init\Action::action()
-	 * @return string
+	 * @return string|null
 	 * An example of result: http://api.omise.co/payments/paym_test_56fuvl1ih89gj1kjzid/authorize
 	 */
-	protected function redirectUrl() {return
+	protected function redirectUrl() {return !$this->s()->_3DS() ? null :
 		// 2016-12-24
 		// «Url for charge authorization using 3-D Secure. Only if return_uri was set.»
 		// https://www.omise.co/charges-api
