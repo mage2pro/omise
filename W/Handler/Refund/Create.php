4ac9e62cf4484ab5a0bca028a1801fe8a5aa0013
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-26 18:42:54 +0300

    no message

--- W/Handler/Refund/Create.php
+++ W/Handler/Refund/Create.php
@@ -13,7 +13,7 @@ final class Create extends \Df\StripeClone\W\Handler implements \Df\StripeClone\
 	 * В валюте заказа (платежа), в формате платёжной системы (копейках).
 	 * @override
 	 * @see \Df\StripeClone\W\IRefund::amount()
-	 * @used-by \Df\StripeClone\W\Strategy\Charge\Refunded::_handle()
+	 * @used-by \Df\StripeClone\W\Strategy\Refunded::_handle()
 	 * @return int
 	 */
 	function amount() {return $this->e()->ro('amount');}
@@ -28,7 +28,7 @@ final class Create extends \Df\StripeClone\W\Handler implements \Df\StripeClone\
 	 * который возвращает @see \Dfe\Omise\Facade\Refund::transId()
 	 * @override
 	 * @see \Df\StripeClone\W\IRefund::eTransId()
-	 * @used-by \Df\StripeClone\W\Strategy\Charge\Refunded::_handle()
+	 * @used-by \Df\StripeClone\W\Strategy\Refunded::_handle()
 	 * @return string
 	 */
 	function eTransId() {return $this->e()->ro('transaction');}
