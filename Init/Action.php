b496d966c2ea9d30b7612bf95987df599bb65bfa
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-11-07 12:50:03 +0300

    1.11.2

--- Init/Action.php
+++ Init/Action.php
@@ -15,9 +15,17 @@ final class Action extends \Df\Payment\Init\Action {
 	 * An example of result: http://api.omise.co/payments/paym_test_56fuvl1ih89gj1kjzid/authorize
 	 */
 	protected function redirectUrl() {return !$this->s()->_3DS() ? null :
-		// 2016-12-24
-		// «Url for charge authorization using 3-D Secure. Only if return_uri was set.»
-		// https://www.omise.co/charges-api
-		$this->m()->chargeNew($this->preconfiguredToCapture())['authorize_uri']
+		/**
+		 * 2016-12-24
+		 * «Url for charge authorization using 3-D Secure. Only if return_uri was set.»
+		 * https://www.omise.co/charges-api
+		 * 2017-11-07
+		 * Note 1. @uses \Df\StripeClone\Method::chargeNew() caches its result.
+		 * Note 2.
+		 * Previously, I called @see \Df\StripeClone\Method::chargeNew() here,
+		 * but now I call @uses \Df\StripeClone\Method::charge(),
+		 * because it additionally adds some tags for Sentry.
+		 */
+		$this->m()->charge($this->preconfiguredToCapture())['authorize_uri']
 	;}	
 }
