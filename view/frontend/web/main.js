0a817b2a2ad27034336533714fc4c2a29430dfaf
Dmitry Fedyuk <dfediuk@gmail.com>
2018-09-29 02:17:52 +0600

    no message

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -9,10 +9,11 @@ return parent.extend({
 	// https://www.omise.co/omise-js-api#createtoken(type,-object,-callback)
 	defaults: {df: {card: {requireCardholder: true}}},
 	/**
-	 * 2016-11-12
-	 * https://www.omise.co/which-credit-cards-does-omise-accept
-	 * 2017-02-05
-	 * The bank card network codes: https://mage2.pro/t/2647
+	 * 2016-11-12 https://www.omise.co/which-credit-cards-does-omise-accept
+	 * 2017-02-05 The bank card network codes: https://mage2.pro/t/2647
+	 * @override
+	 * @see Df_Payment/main::getCardTypes()
+	 * @used-by https://github.com/mage2pro/core/blob/3.9.12/Payment/view/frontend/web/template/card/fields.html#L4
 	 * @returns {String[]}
 	 */
 	getCardTypes: function() {return ['VI', 'MC', 'JCB'];},
