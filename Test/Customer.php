83c771ea2ea32fc0d822199d124a9b6c31184fdb
Dmitry Fedyuk <dfediuk@gmail.com>
2023-01-06 20:33:35 +0000

    https://github.com/mage2pro/core/issues/168

--- Test/Customer.php
+++ Test/Customer.php
@@ -42,7 +42,7 @@ final class Customer extends TestCase {
 	 * мы вынуждены заново делать retrieve для конкретного покупателя.
 	 */
 	function t04():void {
-		array_map(function(array $c) {
+		array_map(function(array $c):void {
 			if ('admin@mage2.pro' === $c['email']) {
 				C::retrieve($c['id'])->destroy();
 			}
